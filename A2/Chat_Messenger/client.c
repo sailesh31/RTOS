d16f79e5598a85aaa6aa65ecb2625f9348a9f261
sailesh31 <32615753+sailesh31@users.noreply.github.com>
2021-02-15 05:07:38 +0530

    Add files via upload
    Commented and Segmented code

--- A2/Chat_Messenger/client.c
+++ A2/Chat_Messenger/client.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
 
 //Headers: Socket related functions
 #include <sys/types.h>
@@ -13,15 +14,54 @@
 #include <arpa/inet.h>
 
 //Server port the client will be connecting to
-#define PORT_NO 4000
+#define PORT_NO 9000
 
 //Max size of the bytes which can be streamed at one go
 #define MAX_BUF_SIZE 1024
 
+//Creating Message Variables for received and transmitted messsages
+char Client_Message[MAX_BUF_SIZE];
+char Received_Message[MAX_BUF_SIZE];
+
+//Message structs
+struct Message
+{
+  char sender_name[MAX_BUF_SIZE];
+  char receiver_name[MAX_BUF_SIZE];
+  char message[MAX_BUF_SIZE];
+  char phone_number[10];
+  int message_type;
+};
+
+int Socket_Descriptor=0;
+
+//Thread based function to receive data from central server
+pthread_t read_thread;
+void *Receive_Message()
+{
+    for (;;)
+    {
+        int message_success;
+        struct Message m;
+        message_success = recv(Socket_Descriptor, &m, sizeof(m), 0);
+        fprintf(stdout,"\33[2K\r%s : %s Sent: ", m.sender_name ,m.message);
+        fflush(stdout);
+    }
+}
+
+//Additional function definations
+int Register_user(char user_name[],char phone_number[],int Socket_Descriptor);
+int Authenticate_user(char user_name[],char phone_number[],int Socket_Descriptor);
+int Send_Private_Message(int a,char Receiver_Name[],char Client_Message[],int Socket_Descriptor,char user_name[]);
+int Send_Group_Message(int a,char group_code[],char Client_Message[],int Socket_Descriptor,char user_name[]);
+int Join_Group(char group_code[],int Socket_Descriptor, char user_name[]);
+
+
+
 int main()
 {
   //Creating a stream socket; Adressfamily_INET domain; Protocol '0' to choose the proper protocol for the given socket.
-  int Socket_Descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  Socket_Descriptor = socket(AF_INET, SOCK_STREAM, 0);
 
   //Checking for the proper creation of socket
   if(Socket_Descriptor==-1)
@@ -32,9 +72,8 @@ int main()
   printf("\nSocket Created\n");
 
   //Defining the structure for server socket
-
   struct hostent *serverIP;
-  char serverdn[] = "ec2-52-66-204-32.ap-south-1.compute.amazonaws.com";
+  char serverdn[] = "ec2-13-127-241-9.ap-south-1.compute.amazonaws.com";
   serverIP = gethostbyname(serverdn);
 
   struct sockaddr_in Server_Address;
@@ -48,44 +87,206 @@ int main()
   int connect_success = connect(Socket_Descriptor,(struct sockaddr *)  &Server_Address, sizeof(Server_Address));
   if(connect_success==-1)
   {
-  		printf("\nUnable to make a socket connection\n");
-  		return -1;
+      printf("\nUnable to make a socket connection\n");
+      return -1;
   }
   printf("\nConnected to server socket\n");
 
-  //Fullduplex data transmission to server on cloud from a client on local system.
-  char Client_Message[MAX_BUF_SIZE];
-  char Received_Message[MAX_BUF_SIZE];
 
-  if(fork()==0)
+  //Getting User Info
+  printf("\nWelcome to My Chat Messenger\n");
+
+  // 1) Existing/New User
+  int user_type = 0;
+  printf("\nIf existing user enter 1 else enter 0\n");
+  scanf("%d",&user_type);
+
+  // 2) Username
+  char user_name[MAX_BUF_SIZE];
+  char phone_number[10];
+  if(user_type==0)
   {
-    while(1){
-    memset(Client_Message, 0, sizeof(Client_Message));
-    printf("\nEnter message to transmit to server: \n");
-    scanf("%[^\n]%*c", Client_Message);
-    int msg_success = send(Socket_Descriptor, Client_Message, sizeof(Client_Message), 0);
-    if(msg_success!=-1)
+    printf("Please enter a username to continue\n");
+    scanf("%s",&user_name);
+    printf("Please enter your mobile number\n");
+    scanf("%s",&phone_number);
+    int reg_success = Register_user(user_name,phone_number,Socket_Descriptor);
+
+    if(reg_success==1)
     {
-      printf("Message Sent\n");
+      printf("Succesfully Registered. You can continue to the Messenger\n");
+    }
+    else if(reg_success==0)
+    {
+      printf("Username/Mobile number in use. Unable to continue\n");
+      exit(0);
+    }
+    else if(reg_success==-1)
+    {
+      printf("Error registering due to Max tenable limit. Please try again\n");
+      exit(0);
+    }
+  }
+
+  else if(user_type==1)
+  {
+    printf("Please enter your username\n");
+    scanf("%s",&user_name);
+    printf("Please enter your mobile number\n");
+    scanf("%s",&phone_number);
+    int auth_success = Authenticate_user(user_name,phone_number,Socket_Descriptor);
+    if(auth_success==1)
+    {
+      printf("Welcome back user. You can continue to the Messenger\n");
     }
     else
     {
-      printf("\nError sending message on the socket\n");
-    }}
- }
- else
- {
-   while(1)
-   {
-    memset(Received_Message, 0, sizeof(Received_Message));
-    int read_success = recv(Socket_Descriptor, Received_Message,MAX_BUF_SIZE,0);
-    if(read_success>0)
-    {
-    printf("Message from Server: %s\n", Received_Message);
-    printf("\n");
+      printf("Unable to authenticate. Please try again\n");
+      exit(0);
     }
   }
- }
+
+
+
+  //For receiving messsages
+  pthread_create(&read_thread, NULL, Receive_Message, NULL);
+
+
+  //User messenging action
+  for(;;)
+  {
+    int user_action;
+    printf("Choose Action : 1)Private Message 2)Group Message 3)Join Group \n");
+    scanf("%d",&user_action);
+    if(user_action==1)
+    {
+      char Receiver_Name[MAX_BUF_SIZE];
+      printf("Enter username to message:\n");
+      scanf("%s", &Receiver_Name);
+      printf("Enter Message to send:\n");
+      scanf("%s",&Client_Message);
+      Send_Private_Message(1,Receiver_Name,Client_Message,Socket_Descriptor,user_name);
+    }
+    else if(user_action==2)
+    {
+      char group_code[MAX_BUF_SIZE];
+      printf("Enter group code\n");
+      scanf("%s",&group_code);
+      printf("Enter Message to send:\n");
+      scanf("%s",&Client_Message);
+      Send_Group_Message(2,group_code,Client_Message,Socket_Descriptor,user_name);
+    }
+    else if(user_action==3)
+    {
+      char group_code[MAX_BUF_SIZE];
+      printf("Enter group code\n");
+      scanf("%s",&group_code);
+      int group_join_success = Join_Group(group_code,Socket_Descriptor,user_name);
+      if(group_join_success==1)
+      {
+        printf("Succesfully Joined the group\n");
+      }
+      else
+      {
+        printf("Wrong code. Unable to join the group\n");
+      }
+    }
+  }
 
   return 0;
+}
+
+
+
+//Defining Custom Functions for the messenger application
+int Register_user(char user_name[],char phone_number[],int Socket_Descriptor)
+{
+  struct Message A1;
+  strcpy(A1.sender_name,user_name);
+  strcpy(A1.phone_number,phone_number);
+  A1.message_type = 00;
+  send(Socket_Descriptor, &A1, sizeof(A1), 0);
+
+  struct Message B1;
+  read(Socket_Descriptor, &B1, sizeof(B1));
+
+  if(B1.message_type==99)
+  {
+    return 1;
+  }
+
+  else if(B1.message_type==88)
+  {
+    return 0;
+  }
+
+  else if(B1.message_type==77)
+  {
+    return -1;
+  }
+}
+
+
+int Authenticate_user(char user_name[],char phone_number[],int Socket_Descriptor)
+{
+  struct Message A2;
+  strcpy(A2.sender_name,user_name);
+  strcpy(A2.phone_number,phone_number);
+  A2.message_type = 01;
+  send(Socket_Descriptor, &A2, sizeof(A2), 0);
+
+  struct Message B2;
+  read(Socket_Descriptor, &B2, sizeof(B2));
+  if(B2.message_type==99)
+  {
+    return 1;
+  }
+
+  else
+  {
+    return 0;
+  }
+
+}
+
+int Send_Private_Message(int a,char Receiver_Name[],char Client_Message[],int Socket_Descriptor,char user_name[])
+{
+  struct Message A3;
+  strcpy(A3.receiver_name,Receiver_Name);
+  strcpy(A3.sender_name,user_name);
+  A3.message_type = 10;
+  strcpy(A3.message,Client_Message);
+  send(Socket_Descriptor, &A3, sizeof(A3), 0);
+}
+
+int Send_Group_Message(int a,char group_code[],char Client_Message[],int Socket_Descriptor,char user_name[])
+{
+  struct Message A4;
+  strcpy(A4.receiver_name,group_code);
+  strcpy(A4.sender_name,user_name);
+  A4.message_type = 11;
+  strcpy(A4.message,Client_Message);
+  send(Socket_Descriptor, &A4, sizeof(A4), 0);
+
+}
+
+int Join_Group(char group_code[],int Socket_Descriptor,char user_name[])
+{
+  struct Message A5;
+  strcpy(A5.sender_name,user_name);
+  A5.message_type = 20;
+  strcpy(A5.message,group_code);
+  send(Socket_Descriptor, &A5, sizeof(A5), 0);
+
+  struct Message B5;
+  read(Socket_Descriptor, &B5, sizeof(B5));
+  if(B5.message_type==99)
+  {
+    return 1;
+  }
+
+  else
+  {
+    return 0;
+  }
 }
--- A2/Chat_Messenger/server.c
+++ A2/Chat_Messenger/server.c
@@ -4,6 +4,7 @@
 #include "string.h"
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
 
 //Headers: Socket related functions
 #include <sys/types.h>
@@ -11,16 +12,161 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 //Server port the client will be connecting to
-#define PORT_NO 4000
+#define PORT_NO 9000
 
 //Max size of the bytes which can be streamed at one go
 #define MAX_BUF_SIZE 1024
 
+//Max users tenable
+#define MAX_MEMBERS 10
+
+//Message structs
+struct Message
+{
+  char sender_name[MAX_BUF_SIZE];
+  char receiver_name[MAX_BUF_SIZE];
+  char message[MAX_BUF_SIZE];
+  char phone_number[10];
+  int message_type;
+};
+
+//Creating Message Variables for received and transmitted messsages
+char Received_Message[MAX_BUF_SIZE];
+char Server_Message[MAX_BUF_SIZE];
+
+//Global Variables Required
+int Socket_Descriptor=0;
+
+int Collection_Usersockets[MAX_MEMBERS] = {0};
+
+int user_count = 0;
+int group_count = 0;
+
+char DataBase_Name[MAX_MEMBERS][1024];
+char DataBase_Number[MAX_MEMBERS][10];
+
+char Group_DB[MAX_MEMBERS][1024];
+
+char *group_code = "123456789";
+
+
+pthread_t client_threads[MAX_MEMBERS];
+pthread_mutex_t registration_lock;
+pthread_t read_thread, write_thread;
+
+
+void *client_handler(void *socket_file_descriptor){
+    int client_file_descriptor = *(int *)socket_file_descriptor;
+    struct Message temp_message;
+    recv(client_file_descriptor, &temp_message, sizeof(temp_message), 0);
+
+    pthread_mutex_lock(&registration_lock);
+
+    if(user_count >= MAX_MEMBERS){
+        printf("At Max tenable limit. Cannot acceot anymore people\n");
+        struct Message temp_reply;
+        temp_reply.message_type = 77;
+        send(client_file_descriptor, &temp_reply, sizeof(temp_reply), 0);
+        close(client_file_descriptor);
+        pthread_cancel(client_threads[user_count]);
+    }
+
+    else if(temp_message.message_type==01)
+     {
+       struct Message temp_reply;
+       for(int i=0;i<user_count;i++)
+       {
+         if(strcmp(DataBase_Name[user_count],temp_message.sender_name) == 0 )
+         {
+           if(strcmp(DataBase_Number[user_count],temp_message.phone_number) == 0 )
+           {
+             temp_reply.message_type = 99;
+           }
+         }
+       }
+       send(client_file_descriptor, &temp_reply, sizeof(temp_reply), 0);
+     }
+
+     else if(temp_message.message_type==00)
+     {
+       struct Message temp_reply;
+       strcpy(DataBase_Name[user_count],temp_message.sender_name);
+       strcpy(DataBase_Number[user_count],temp_message.phone_number);
+       temp_reply.message_type = 99;
+       send(client_file_descriptor, &temp_reply, sizeof(temp_reply), 0);
+       Collection_Usersockets[user_count] = client_file_descriptor;
+       user_count+=1;
+     }
+
+     else if(temp_message.message_type==20)
+     {
+       struct Message temp_reply;
+       if(strcmp(temp_message.message,group_code)==0)
+       {
+         temp_reply.message_type = 99;
+         send(client_file_descriptor, &temp_reply, sizeof(temp_reply), 0);
+       }
+       strcpy(Group_DB[group_count],temp_message.sender_name);
+       group_count+=1;
+     }
+
+     pthread_mutex_unlock(&registration_lock);
+
+     struct Message communication;
+
+     while(recv(client_file_descriptor, &communication, sizeof(communication), 0))
+     {
+        if(communication.message_type==10)
+        {
+          for(int i = 0; i < user_count; i++)
+          {
+                if(strcmp(DataBase_Name[i], communication.receiver_name) == 0)
+                {
+                    send(Collection_Usersockets[i], &communication, sizeof(communication), 0);
+                }
+          }
+        }
+
+        else if(communication.message_type==11)
+        {
+          for(int i = 0; i < group_count; i++)
+          {
+                if(strcmp(Group_DB[i], communication.sender_name) != 0)
+                {
+                    send(Collection_Usersockets[i], &communication, sizeof(communication), 0);
+                }
+          }
+        }
+
+     }
+
+       // printf("%s Left the group\n",temp_message.sender_name);
+       pthread_mutex_lock(&registration_lock);
+       int exit_member_id;
+       for(int i=0;i<MAX_MEMBERS;i++){
+           if(strcmp(temp_message.sender_name, DataBase_Name[i]) == 0){
+               exit_member_id = 1;
+               break;
+           }
+       }
+       for(exit_member_id;exit_member_id<user_count;exit_member_id++){
+           Collection_Usersockets[exit_member_id] = Collection_Usersockets[exit_member_id + 1];
+           strcpy(DataBase_Name[exit_member_id],DataBase_Number[exit_member_id + 1]);
+           strcpy(DataBase_Name[exit_member_id],DataBase_Number[exit_member_id + 1]);
+       }
+       user_count--;
+       pthread_mutex_unlock(&registration_lock);
+
+
+
+}
+
 
 int main()
 {
-  int Socket_Descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  Socket_Descriptor = socket(AF_INET, SOCK_STREAM, 0);
 
   //Checking for the proper creation of socket
   if(Socket_Descriptor==-1)
@@ -55,7 +201,7 @@ int main()
   printf("\nSocket binding complete\n");
 
   //Listening on the socket for connections
-  int listen_success = listen(Socket_Descriptor, 3);
+  int listen_success = listen(Socket_Descriptor, MAX_MEMBERS);
   if(listen_success == -1)
   {
          printf("\nListen operation not succesful on the socket\n");
@@ -63,50 +209,24 @@ int main()
   }
   printf("\nListening\n");
 
-  //Accpting client connection
-  int New_socket_descriptor = accept(Socket_Descriptor, (struct sockaddr *) &Address, (socklen_t *) &Addresslen);
-  if(New_socket_descriptor==-1)
+  int temp = 0;
+  struct sockaddr_in temp_serv;
+  for(;;)
   {
-         printf("\n Unable to accept request from client\n");
-  }
-  printf("\nSuccessful Connection established with client\n");
+    int *connection_file_descriptor = malloc(sizeof(int));
+   *connection_file_descriptor = accept(Socket_Descriptor, (struct sockaddr*)&temp_serv, &temp);
 
-  //Fullduplex data transmission to client from a server on cloud.
-  char Received_Message[MAX_BUF_SIZE];
-  char Server_Message[MAX_BUF_SIZE];
-  if(fork()==0)
-  {
-    while(1)
-    {
-      memset(Received_Message, 0, sizeof(Received_Message));
-      int read_success = recv(New_socket_descriptor, Received_Message,MAX_BUF_SIZE,0);
-      if(read_success>0)
-      {
-        printf("Message from Client: %s\n", Received_Message);
-        printf("\n");
-      }
-    }
+   if(*connection_file_descriptor < 0){
+       printf("Client connection failed\n");
+       exit(0);
+   }
+
+   printf("connection succesful\n")
+   pthread_create(&client_threads[user_count], NULL, client_handler, (void *)connection_file_descriptor);
+
   }
 
-  else
-  {
-    while(1)
-    {
-      memset(Server_Message, 0, sizeof(Server_Message));
-      printf("\nEnter message to transmit to client: \n");
-      scanf("%[^\n]%*c", Server_Message);
-      int msg_success = send(New_socket_descriptor, Server_Message, sizeof(Server_Message), 0);
-      if(msg_success!=-1)
-      {
-        printf("Message Sent\n");
-      }
-      else
-      {
-        printf("\nError sending message on the socket\n");
-      }
 
-    }
-  }
 
 
   return 0;
